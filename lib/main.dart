46911e78c2425fc1507d17bf5c636ab00ff0fce7
Suryakhattri21 <81793254+Suryakhattri21@users.noreply.github.com>
2021-04-02 11:10:53 +0545

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:buddha_shop/details_screen.dart';
+import 'package:buddha_shop/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: DetailsScreen(),
+  ));
+}
